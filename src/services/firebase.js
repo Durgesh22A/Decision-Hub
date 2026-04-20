659900adf38bc905a342eb937b8018829707b624
Durgesh22A <durgesh.25bcs10703@sst.scaler.com>
2026-04-21 00:50:58 +0530

    Update Firebase configuration to use hardcoded values for apiKey and other parameters for local development

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDTrK7mEt9Y-LrGFBpRW7I1Pfq-ufnouP8",
+  authDomain: "decisionhub-8fe09.firebaseapp.com",
+  projectId: "decisionhub-8fe09",
+  storageBucket: "decisionhub-8fe09.firebasestorage.app",
+  messagingSenderId: "433070850680",
+  appId: "1:433070850680:web:107f01768e0aab718c238d"
 };
 
 const app = initializeApp(firebaseConfig);
